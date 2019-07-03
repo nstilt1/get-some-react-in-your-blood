75ea2ebf73406c75a3250d1dcad012c39eab3be0
Noah Stiltner <nstilt1@lsu.edu>
2019-07-03 09:37:57 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Login from './containers/login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+    route: '/login',
+    component: 'Login'
+    }
+
+  logIn = () => {
+    console.log("Logged in");
+    this.setState({
+      
+      isLoggedIn: true,
+      route: '/home',
+      component: 'Home'
+    });
+  };
+  
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route exact path={this.state.route} render={props => (
+                <React.Fragment>
+                  <Login logIn={this.logIn} objects={this.state.formDetails} />
+                </React.Fragment>
+              )}/>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
